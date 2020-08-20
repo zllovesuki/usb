88143b090024f0b862ec72d11598e97cbf79198c
Rachel Chen <me@rachelchen.me>
2020-08-20 15:46:03 -0700

    remove appending report_id on windows
    change path

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/karalabe/usb
+module github.com/zllovesuki/usb
 
 go 1.12
--- hid_enabled.go
+++ hid_enabled.go
@@ -25,7 +25,6 @@ import "C"
 
 import (
 	"errors"
-	"runtime"
 	"sync"
 	"unsafe"
 )
@@ -121,15 +120,8 @@ func (dev *hidDevice) Write(b []byte) (int, error) {
 	if device == nil {
 		return 0, ErrDeviceClosed
 	}
-	// Prepend a HID report ID on Windows, other OSes don't need it
-	var report []byte
-	if runtime.GOOS == "windows" {
-		report = append([]byte{0x00}, b...)
-	} else {
-		report = b
-	}
 	// Execute the write operation
-	written := int(C.hid_write(device, (*C.uchar)(&report[0]), C.size_t(len(report))))
+	written := int(C.hid_write(device, (*C.uchar)(&b[0]), C.size_t(len(b))))
 	if written == -1 {
 		// If the write failed, verify if closed or other error
 		dev.lock.Lock()
